e9a77ff85383d3ba88ad6dad08e9bbeb78322f33
Ryan Omasta <git@ryand.ca>
2026-01-12 13:23:33 -0700

    Blink some LEDs.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -52,8 +52,37 @@
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
+    HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    // Configure HSI as system clock
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;       // no division
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE; // skip PLL for simplicity
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	Error_Handler();
+
+    // Set HSI as system clock source
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
+                                  RCC_CLOCKTYPE_PCLK1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+	Error_Handler();
+}
 /* USER CODE END 0 */
+
+void Error_Handler(void)
+{
+
+}
 
 /**
  * @brief  The application entry point.
@@ -61,7 +90,15 @@
  */
 int main(void)
 {
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  projectMain();
+
+  while (1) { }
 }
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- Project/projectMain.cpp
+++ Project/projectMain.cpp
@@ -35,11 +35,32 @@ private:
 
 void projectMain()
 {
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = GPIO_PIN_6;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    GPIO_InitTypeDef GPIO_InitStructLED = {0};
+    GPIO_InitStructLED.Pin = GPIO_PIN_0;
+    GPIO_InitStructLED.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructLED.Pull = GPIO_NOPULL;
+    GPIO_InitStructLED.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStructLED);
+
     Led led(GPIOC, GPIO_PIN_6);
+    Led circuitLed(GPIOA, GPIO_PIN_0);
 
     while (true)
     {
         led.toggle();
         HAL_Delay(2000);
+
+	circuitLed.toggle();
+	HAL_Delay(2000);
     }
 }
